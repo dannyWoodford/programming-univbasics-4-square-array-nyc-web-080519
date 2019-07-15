3b9a207a121c5434f7706702f27f57496f720ee0
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-15 17:36:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    counter += 1
     new_array.push(array[counter]**2)
+    counter += 1
   end
   return new_array
 end
